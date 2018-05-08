726f31ae887bc2047866a354973224f2dc384626
Jon McLean <Jon McLean>
2018-05-08 13:53:42 -0700

    Filtered out fish w/ scales < 1 mm

--- scripts/week 7 example.R
+++ scripts/week 7 example.R
@@ -5,4 +5,7 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 #this addsa a category to the table that categorizes fish by size
 library(dplyr)
 fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
+  mutate(length_cat = ifelse(length > 200, "big", "small")) %>% 
+  filter(scalelength > 1)
+
+
