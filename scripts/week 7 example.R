94ea3ad436bd845b6643fc85ad34dfc932c6a724
Jon McLean <Jon McLean>
2018-05-08 13:44:59 -0700

    Added dplyr comment to code

--- scripts/week 7 example.R
+++ scripts/week 7 example.R
@@ -2,7 +2,7 @@
 
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
-
+#this addsa a category to the table that categorizes fish by size
 library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
