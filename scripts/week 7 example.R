a5cc71ac3f7af3004215c7ac0b6e52792fc00f91
Jon McLean <Jon McLean>
2018-05-08 13:57:45 -0700

    Revised data after renaming

--- scripts/week 7 example.R
+++ scripts/week 7 example.R
@@ -1,6 +1,6 @@
 
 
-fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
+fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 
 #this addsa a category to the table that categorizes fish by size
 library(dplyr)
